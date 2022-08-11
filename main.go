535c812aa79716c1613ba0f15f0e2a0fd3aa6bdb
Thomas M Guarnieri <thomasmguarnieri@gmail.com>
2022-08-10 22:24:14 -0300

    increased buffer size

--- main.go
+++ main.go
@@ -11,36 +11,24 @@ import (
 	"time"
 )
 
+const bufSize = 1024 * 1024
+
 func main() {
+	var rows [][]interface{}
+	buf := make([]byte, bufSize)
 	file := utils.ReadFile(fmt.Sprintf("%s", os.Args[1]))
 	// Changes on the string below may break this program
 	rg := regexp.MustCompile(`(.+) - - \[(.+)] "(.+?) /(.+)" (\d{3})`)
-	var rows [][]interface{}
 
 	// Close file when main function finishes
 	defer file.Close()
 
 	// Read file line by line
 	scn := bufio.NewScanner(file)
+	scn.Buffer(buf, bufSize)
+
 	for scn.Scan() {
-		// Needed data:
-		// ipAddress | requestType | requestPath | responseStatusCode | accessDate
-		match := rg.FindStringSubmatch(scn.Text())
-		if match != nil {
-			sc, err := strconv.Atoi(match[5])
-			if err != nil {
-				fmt.Println("Fail on string to int conversion")
-				panic(err)
-			}
-
-			dt, err := time.Parse("02/Jan/2006:15:04:05 -0700", match[2])
-			if err != nil {
-				fmt.Println("Fail on string to date conversion")
-				panic(err)
-			}
-
-			rows = append(rows, []interface{}{match[1], match[3], match[4], sc, dt})
-		}
+		rows = append(rows, extractLogLineData(scn.Text(), rg))
 	}
 
 	fmt.Println("Finished processing file")
@@ -49,4 +37,28 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Finished saving data to database")
+}
+
+func extractLogLineData(s string, r *regexp.Regexp) []interface{} {
+	// Needed data:
+	// ipAddress | requestType | requestPath | responseStatusCode | accessDate
+	match := r.FindStringSubmatch(s)
+	if match != nil {
+		// Converts responseStatusCode to integer
+		sc, err := strconv.Atoi(match[5])
+		if err != nil {
+			fmt.Println("Fail on string to int conversion")
+			panic(err)
+		}
+
+		dt, err := time.Parse("02/Jan/2006:15:04:05 -0700", match[2])
+		if err != nil {
+			fmt.Println("Fail on string to date conversion")
+			panic(err)
+		}
+
+		return []interface{}{match[1], match[3], match[4], sc, dt}
+	}
+
+	return nil
 }
