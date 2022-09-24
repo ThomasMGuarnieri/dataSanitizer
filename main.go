319dae098072100b31232797a4dd4aff8b39f75e
Thomas M Guarnieri <thomasmguarnieri@gmail.com>
2022-09-24 14:28:07 -0300

    multithread read file proccess

--- main.go
+++ main.go
@@ -1,74 +1,78 @@
 package main
 
 import (
-	"bufio"
 	"dataSanitizer/db"
-	"dataSanitizer/utils"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 )
 
+const threads int = 4
+
+var (
+	wg   = sync.WaitGroup{}
+	rows [][]interface{}
+)
+
+func extractLogLineData(s chan string, r *regexp.Regexp) {
+	// Needed data:
+	// ipAddress | requestType | requestPath | responseStatusCode | accessDate
+	for l := range s {
+		match := r.FindStringSubmatch(l)
+		if match != nil {
+			// Converts responseStatusCode to integer
+			sc, err := strconv.Atoi(match[5])
+			if err != nil {
+				fmt.Println("Fail on string to int conversion")
+				panic(err)
+			}
+
+			dt, err := time.Parse("02/Jan/2006:15:04:05 -0700", match[2])
+			if err != nil {
+				fmt.Println("Fail on string to date conversion")
+				panic(err)
+			}
+
+			rows = append(rows, []interface{}{match[1], match[3], match[4], sc, dt})
+		}
+	}
+	wg.Done()
+}
+
 func main() {
-	var rows [][]interface{}
-	f := utils.ReadFile(fmt.Sprintf("%s", os.Args[1]))
+
+	f, err := os.ReadFile(fmt.Sprintf("%s", os.Args[1]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Changes on the string below may break this program
 	rg := regexp.MustCompile(`(.+) - - \[(.+)] "(.+?) /(.+)" (\d{3})`)
 
-	defer f.Close()
+	txt := string(f)
 
-	r := bufio.NewReader(f)
-
-	for {
-		var i []interface{}
-		s, err := r.ReadString('\n')
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
-		}
-
-		if err == io.EOF {
-			break
-		}
-
-		i = extractLogLineData(s, rg)
-
-		if i != nil {
-			rows = append(rows, i)
-		}
+	inputCh := make(chan string, 300)
+	for i := 0; i < threads; i++ {
+		go extractLogLineData(inputCh, rg)
 	}
 
+	wg.Add(threads)
+
+	for _, l := range strings.Split(txt, "\n") {
+		inputCh <- l
+	}
+	close(inputCh)
+	wg.Wait()
 	fmt.Println("Finished processing file")
-	err := db.BulkLogDataInsert(rows)
+
+	err = db.BulkLogDataInsert(rows)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Finished saving data to database")
-}
-
-func extractLogLineData(s string, r *regexp.Regexp) []interface{} {
-	// Needed data:
-	// ipAddress | requestType | requestPath | responseStatusCode | accessDate
-	match := r.FindStringSubmatch(s)
-	if match != nil {
-		// Converts responseStatusCode to integer
-		sc, err := strconv.Atoi(match[5])
-		if err != nil {
-			fmt.Println("Fail on string to int conversion")
-			panic(err)
-		}
-
-		dt, err := time.Parse("02/Jan/2006:15:04:05 -0700", match[2])
-		if err != nil {
-			fmt.Println("Fail on string to date conversion")
-			panic(err)
-		}
-
-		return []interface{}{match[1], match[3], match[4], sc, dt}
-	}
-
-	return nil
 }
